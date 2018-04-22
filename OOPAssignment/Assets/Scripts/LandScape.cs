4d4ef19871303afa10140d0e99706397a3dc68e4
Will <williamcarey1998@gmail.com>
2018-04-22 16:57:24 +0100

    added Scripts

---
+++ OOPAssignment/Assets/Scripts/LandScape.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandScape : MonoBehaviour
+{
+    public float squareArea;
+	// Use this for initialization
+	void Start ()
+    {
+        transform.position.Set(0, 0, 0);
+        
+        transform.localScale = new Vector3(squareArea, 1, squareArea);
+	}
+}
