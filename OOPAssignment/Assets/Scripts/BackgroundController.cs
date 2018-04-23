e9c4f0806da2d9522c66d206c8e5c543b85ea678
Will <williamcarey1998@gmail.com>
2018-04-23 15:09:12 +0100

    Trying a new path

---
+++ OOPAssignment/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+    public Material [] background;
+
+    void Start()
+    {
+        background = Resources.LoadAll<Material>("");
+    }
+    // Update is called once per frame
+    void Update () {
+        RenderSettings.skybox = background[3];
+		
+	}
+}
