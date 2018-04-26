598fe416bd2a2298e60c7d44e68948387df6f21d
Will <williamcarey1998@gmail.com>
2018-04-26 18:38:13 +0100

    Got bullets working

---
+++ OOPAssignment/Assets/Scripts/BulletMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour {
+
+    public float speed;
+
+    public Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+
+        rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.velocity = transform.forward * speed;
+	}
+
+    private void Update()
+    {
+        Destroy(gameObject, 3);
+    }
+}
--- OOPAssignment/Assets/Scripts/PlayerController.cs
+++ OOPAssignment/Assets/Scripts/PlayerController.cs
@@ -7,27 +7,29 @@ public class PlayerController : MonoBehaviour
     public GameObject shot;
     public Transform shotSpawn;
     public float rotationSpeed = 150f;
-    public float moveSpeed = 20.0f;
+    public float moveSpeed = 10.0f;
     public float nextFire, fireRate;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
-            Fire();
-
-            //Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
         }
+    }
 
-        if (Input.GetKey(KeyCode.W) && moveSpeed < 500f)
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W) && moveSpeed < 250f)
         {
-            moveSpeed += 50;
+            moveSpeed += 5;
         }
 
         if (Input.GetKey(KeyCode.S) && moveSpeed > 0f)
         {
-            moveSpeed -= 50;
+            moveSpeed -= 5;
         }
 
         if (Input.GetKey(KeyCode.A))
@@ -55,7 +57,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.Z))
         {
             //spin left
-            transform.Rotate(0,0, rotationSpeed * Time.deltaTime);
+            transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
         }
 
 
@@ -66,10 +68,7 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.Translate(0, 0, moveSpeed * Time.deltaTime);
-    }
 
-    private void FixedUpdate()
-    {
         if (moveSpeed > 500)
             moveSpeed = 500;
 
@@ -77,20 +76,4 @@ public class PlayerController : MonoBehaviour
             moveSpeed = 0;
     }
 
-    void Fire()
-    {
-        // Create the Bullet from the Bullet Prefab
-        var bullet = (GameObject)Instantiate(
-            shot,
-            shotSpawn.position,
-            shotSpawn.rotation);
-
-        bullet.transform.Rotate(90, 0, 0);
-
-        // Add velocity to the bullet
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * (moveSpeed*2);
-
-        // Destroy the bullet after 2 seconds
-        Destroy(bullet, 2.0f);
-    }
 }   
